97d61a6515f3949b2220d4f33f5ddfad8f894e33
Joe Johnson <joe.wendell.johnson@gmail.com>
2019-02-07 14:47:29 -0600

    Add project files.

---
+++ PreWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace February2019Cards
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Here's a comment.
+            //Here's another comment.
+            //Here's a third comment.
+            Deck deck = new Deck();
+
+            deck.Shuffle();
+            Console.WriteLine(deck);
+
+            Deck otherDeck = new Deck();
+            otherDeck.Shuffle();
+
+            Console.WriteLine(otherDeck);
+            Console.ReadLine();
+        }
+    }
+}
